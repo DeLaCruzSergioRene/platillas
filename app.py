af42c470174f8e2d3acd785443315fcdfba03628
DeLaCruzRodriguezSergio <23308060610508@cetis61.edu.mx>
2025-10-07 11:31:07 -0500

    arreglado

--- app.py
+++ app.py
@@ -6,27 +6,21 @@ app = Flask(__name__)
 def index():
     heroes = ["Motoyasu (Héroe De La Lanza)", "Ren (Héroe De La Espada)", "Itsuki (Héroe Del Arco)"]
     autor = "Naofumi Iwatani (Héroe Del Escudo)"
-    
     return render_template("index.html", nombre = autor, heroes = heroes)
-if __name__ == "__main__":
-    app.run(debug=True)
     
     
 @app.route("/2")
 def index():
     habilidades_escudo = ["Escudo de Aire", "Escudo Meteoro", "Prisión de Escudo"]
     autor = "Naofumi Iwatani (Héroe Del Escudo)"
-    
     return render_template("index2.html", nombre = autor, habilidades_escudo = habilidades_escudo)
-if __name__ == "__main__":
-    app.run(debug=True)
     
     
 @app.route("/3")
 def index():
     caracteristicas = ["Escudo De Aire crea un escudo traslucido de color verde en el aire que protege donde se invoque y resiste cierto daño.", "Escudo Meteoro usa los SP del usuario para resistir todo el daño que sea necesario, consumiendo mas y mas mientras se siga usando.", "Prisión De Escudo crea una esfera hecha de escudos que protege o encierra a quien este dentro según su uso."]
     autor = "Naofumi Iwatani (Héroe Del Escudo)"
-    
     return render_template("index3.html", nombre = autor, caracteristicas = caracteristicas)
+
 if __name__ == "__main__":
     app.run(debug=True)
